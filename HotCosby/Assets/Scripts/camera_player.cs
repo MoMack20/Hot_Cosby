9477f651ee45b3d17252622350e9b77e3bbb73f8
Maurice <mcphersonm@mymail.shawnee.edu>
2016-11-09 23:30:54 -0500

    player controller and camera

---
+++ HotCosby/Assets/Scripts/camera_player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_player : MonoBehaviour {
+
+	private Transform target;
+	// Use this for initialization
+	void Start () {
+		target = transform.parent;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (target);
+	}
+}
---
+++ HotCosby/Assets/Scripts/controller_player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controller_player : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed = 0;
+	private int forward = 0;
+	private int right = 0;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody> ().velocity = (new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"))) * Time.deltaTime * speed;
+	}
+}
